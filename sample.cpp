2664bcaa3d59d40e912349eed651d45d3126086b
Aditya Singh <adityasinghwolvieait@gmail.com>
2021-10-29 16:20:34 +0530

    UPDATE 2.0

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout<<"Hello, world!"<<endl;??This prints Hello,World
+    cout<<"Hello, scooby!"<<endl;//This prints Hello,World
     return 0;
 }
